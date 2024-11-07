5c2b1759f30bc82d7d9022b9ce75310872d0c884
Clement Viel <vielclement@gmail.com>
2024-11-07 15:26:13 +0100

    add xargo command line to build script

--- rust-kernel/build.sh
+++ rust-kernel/build.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 #
+xargo build --target arm-none-eabihf
+sleep 1
 arm-none-eabi-gcc -mcpu=arm1176jzf-s -fpic -ffreestanding -c boot.S -o boot.o
 arm-none-eabi-gcc -T linker.ld -o myos.elf -ffreestanding -O2 -nostdlib boot.o $1
 #arm-none-eabi-gcc -mcpu=arm1176jzf-s -fpic -ffreestanding -std=gnu99 -c kernel_test.c -o kernel.o -O2 -Wall -Wextra
